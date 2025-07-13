5edb8a69693e4554d292a55eb6681c2cae328e7c
Mubeen Mohammed <mubeen.mohammed@mail.mcgill.ca>
2025-07-13 18:25:27 -0400

    valid_sudoku

---
+++ valid_sudoku.py
@@ -0,0 +1,120 @@
+# Valid Sudoku
+# You are given a 9 x 9 Sudoku board board. A Sudoku board is valid if the following rules are followed:
+
+# Each row must contain the digits 1-9 without duplicates.
+# Each column must contain the digits 1-9 without duplicates.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without duplicates.
+# Return true if the Sudoku board is valid, otherwise return false
+
+# Note: A board does not need to be full or be solvable to be valid.
+
+# Example 1:
+
+
+
+# Input: board = 
+# [["1","2",".",".","3",".",".",".","."],
+#  ["4",".",".","5",".",".",".",".","."],
+#  [".","9","8",".",".",".",".",".","3"],
+#  ["5",".",".",".","6",".",".",".","4"],
+#  [".",".",".","8",".","3",".",".","5"],
+#  ["7",".",".",".","2",".",".",".","6"],
+#  [".",".",".",".",".",".","2",".","."],
+#  [".",".",".","4","1","9",".",".","8"],
+#  [".",".",".",".","8",".",".","7","9"]]
+
+# Output: true
+# Example 2:
+
+# Input: board = 
+# [["1","2",".",".","3",".",".",".","."],
+#  ["4",".",".","5",".",".",".",".","."],
+#  [".","9","1",".",".",".",".",".","3"],
+#  ["5",".",".",".","6",".",".",".","4"],
+#  [".",".",".","8",".","3",".",".","5"],
+#  ["7",".",".",".","2",".",".",".","6"],
+#  [".",".",".",".",".",".","2",".","."],
+#  [".",".",".","4","1","9",".",".","8"],
+#  [".",".",".",".","8",".",".","7","9"]]
+
+# Output: false
+# Explanation: There are two 1's in the top-left 3x3 sub-box.
+
+# Constraints:
+
+# board.length == 9
+# board[i].length == 9
+# board[i][j] is a digit 1-9 or '.'.
+
+from typing import List
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # Thinking brute force
+        # Step 1: Go through each row first and check for duplicates, if found return false else move to step 2
+        # Step 2: Go through each column and check for duplicates, if found return false else move to step 3
+        # Step 3: Check each 3 * 3 sub boxes and check for duplicates, if found return false else return true
+        
+        # Step 1: Check the rows
+        for i in range(len(board)):
+            row_duplicates = {}
+            row = board[i]
+            for j in range(len(row)):
+                if row[j] == ".":
+                    continue
+                if row[j] in row_duplicates:
+                    return False
+                row_duplicates[row[j]] = 1
+        
+        # Step 2: Check the columns
+        for i in range(9):
+            col_duplicates = {}
+            for j in range(9):
+                val = board[j][i]
+                if val == ".":
+                    continue
+                if val in col_duplicates:
+                    return False
+                col_duplicates[val] = 1
+
+        # Step 3: Check the sub-boxes
+        for square in range(9):
+            seen = set()
+            for i in range(3):
+                for j in range(3):
+                    row = (square//3) * 3 + i
+                    col = (square % 3) * 3 + j
+                    if board[row][col] == ".":
+                        continue
+                    if board[row][col] in seen:
+                        return False
+                    seen.add(board[row][col])
+        return True
+
+
+        
+
+# This is another solution that uses a dictionary to keep track of the seen numbers in each row, column, and sub-box and checks for duplicates.
+
+from collections import defaultdict
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)  
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if ( board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]):
+                    return False
+
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
+        return True
